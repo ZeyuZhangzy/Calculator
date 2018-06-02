1d5d74535f6761bacd32ba62fb687bdc04bc843f
Zeyu Zhang <zhangzeyu1@hotmail.com>
2018-06-02 22:04:37 +0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,4 @@
-import java.awt.*;
+mport java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.Vector;
@@ -22,4 +22,17 @@ public class Calculator {
     int k1 = 1;
     // 开关2用于记录符号键的次数，如果 k2>1， 说明进行的是 2+3-9+8 这样的多符号运算
     int k2 = 1;
+    // 开关3用于识别str1 是否可以被清0 ，等于1时可以，不等于1时不能被清0
+    int k3 = 1;
+    // 开关4用于识别 str2 是否可以被清0
+    int k4 = 1;
+    // 开关5用于控制小数点可否被录入，等于1时可以， 不为1时，输入的小数点被丢掉
+    int k5 = 1;
+    // stone的作用类似于寄存器， 用于记录是否连续按下符号键
+    JButton store;
     
+    @SuppressWarnings("rawtypes")
+    Vector vt = new Vector(20, 10);
+    
+    // 声明各个UI组件对象并初始化
+    JFrame 
