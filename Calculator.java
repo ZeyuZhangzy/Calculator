c9f337acd01d37d7ace70737afa99ec958f15f9d
Zeyu Zhang <zhangzeyu1@hotmail.com>
2018-06-03 22:19:22 +0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -108,4 +108,266 @@ public class Calculator {
 
         // 设置主窗口出现在屏幕上的位置
         frame.setLocation(300, 200);
+        // 设置窗口不能调大小
+        frame.setResizable(flase);
+        frame.getContentPane().setLayout(new BordenLayout());
+        frame.getContentPane().add(pan2,BordertLayout.NORTH);
+        frame.getContentPane().add(pan,BorderLayout.CENTER);
         
+        frame.pack();
+        frame.setVisible(true);
+        
+        // 事件处理程序
+        
+        // 数字键
+        class Listener implements ActionListener {
+            @SuppressWarnings("unchecked")
+            public void actionPerformed(ActionEvent e) {
+                String ss = ((JButton) e.getSource()).getText();
+                store = (JButton) e.getSource();
+                vt.add(store);
+                if (k1 == 1) {
+                    if (k3 == 1) {
+                        str1 = "";
+
+                        // 还原开关k5状态
+                        k5 = 1;
+                    }
+                    str1 = str1 + ss;
+
+                    k3 = k3 + 1;
+
+                    // 显示结果
+                    result_TextField.setText(str1);
+
+                } else if (k1 == 2) {
+                    if (k4 == 1) {
+                        str2 = "";
+
+                        // 还原开关k5状态
+                        k5 = 1; 
+                    }
+                    str2 = str2 + ss;
+                    k4 = k4 + 1;
+                    result_TextField.setText(str2);
+                }
+
+            }
+        }
+
+        // 输入的运算符号的处理
+        class Listener_signal implements ActionListener {
+            @SuppressWarnings("unchecked")
+            public void actionPerformed(ActionEvent e) {
+                String ss2 = ((JButton) e.getSource()).getText();
+                store = (JButton) e.getSource();
+                vt.add(store);
+
+                if (k2 == 1) {
+                    // 开关 k1 为 1 时向数 1 写输入值，为2时向数2写输入值。
+                    k1 = 2;
+                    k5 = 1;
+                    signal = ss2;
+                    k2 = k2 + 1;// 按符号键的次数
+                } else {
+                    int a = vt.size();
+                    JButton c = (JButton) vt.get(a - 2);
+
+                    if (!(c.getText().equals("+"))
+                            && !(c.getText().equals("-"))
+                            && !(c.getText().equals("*"))
+                            && !(c.getText().equals("/")))
+
+                    {
+                        cal();
+                        str1 = result;
+                        // 开关 k1 为 1 时，向数 1 写值，为2时向数2写
+                        k1 = 2;
+                        k5 = 1;
+                        k4 = 1;
+                        signal = ss2;
+                    }
+                    k2 = k2 + 1;
+
+                }
+
+            }
+        }
+
+        // 清除键的逻辑（Clear）
+        class Listener_clear implements ActionListener {
+            @SuppressWarnings("unchecked")
+            public void actionPerformed(ActionEvent e) {
+                store = (JButton) e.getSource();
+                vt.add(store);
+                k5 = 1;
+                k2 = 1;
+                k1 = 1;
+                k3 = 1;
+                k4 = 1;
+                str1 = "0";
+                str2 = "0";
+                signal = "";
+                result = "";
+                result_TextField.setText(result);
+                vt.clear();
+            }
+        }
+
+        // 等于键的逻辑
+        class Listener_dy implements ActionListener {
+            @SuppressWarnings("unchecked")
+            public void actionPerformed(ActionEvent e) {
+
+                store = (JButton) e.getSource();
+                vt.add(store);
+                cal();
+
+                // 还原各个开关的状态
+                k1 = 1; 
+                k2 = 1;
+                k3 = 1;
+                k4 = 1;
+
+                str1 = result; 
+            }
+        }
+
+        // 小数点的处理
+        class Listener_xiaos implements ActionListener {
+            @SuppressWarnings("unchecked")
+            public void actionPerformed(ActionEvent e) {
+                store = (JButton) e.getSource();
+                vt.add(store);
+                if (k5 == 1) {
+                    String ss2 = ((JButton) e.getSource()).getText();
+                    if (k1 == 1) {
+                        if (k3 == 1) {
+                            str1 = "";
+                            // 还原开关k5状态
+                            k5 = 1; 
+                        }
+                        str1 = str1 + ss2;
+
+                        k3 = k3 + 1;
+
+                        // 显示结果
+                        result_TextField.setText(str1);
+
+                    } else if (k1 == 2) {
+                        if (k4 == 1) {
+                            str2 = "";
+                            // 还原开关k5的状态
+                            k5 = 1;
+                        }
+                        str2 = str2 + ss2;
+
+                        k4 = k4 + 1;
+
+                        result_TextField.setText(str2);
+                    }
+                }
+
+                k5 = k5 + 1;
+            }
+        }
+
+        // 注册各个监听器，即绑定事件响应逻辑到各个UI组件上
+        Listener_dy jt_dy = new Listener_dy();
+
+        // 监听数字键
+        Listener jt = new Listener();
+        // 监听符号键
+        Listener_signal jt_signal = new Listener_signal();
+        // 监听清除键
+        Listener_clear jt_c = new Listener_clear(); 
+        // 监听小数点键
+        Listener_xiaos jt_xs = new Listener_xiaos();
+
+        button7.addActionListener(jt);
+        button8.addActionListener(jt);
+        button9.addActionListener(jt);
+        button_chu.addActionListener(jt_signal);
+        button4.addActionListener(jt);
+        button5.addActionListener(jt);
+        button6.addActionListener(jt);
+        button_cheng.addActionListener(jt_signal);
+        button1.addActionListener(jt);
+        button2.addActionListener(jt);
+        button3.addActionListener(jt);
+        button_jian.addActionListener(jt_signal);
+        button0.addActionListener(jt);
+        button_Dian.addActionListener(jt_xs);
+        button_dy.addActionListener(jt_dy);
+        button_jia.addActionListener(jt_signal);
+        clear_Button.addActionListener(jt_c);
+
+        // 窗体关闭事件的响应程序
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+    }
+
+    // 计算逻辑
+    public void cal() {
+        // 操作数1
+        double a2;
+        // 操作数2
+        double b2;
+        // 运算符
+        String c = signal;
+        // 运算结果
+        double result2 = 0;
+
+        if (c.equals("")) {
+            result_TextField.setText("Please input operator");
+
+        } else {
+            // 手动处理小数点的问题
+            if (str1.equals("."))
+                str1 = "0.0";
+            if (str2.equals("."))
+                str2 = "0.0";
+            a2 = Double.valueOf(str1).doubleValue();
+            b2 = Double.valueOf(str2).doubleValue();
+
+            if (c.equals("+")) {
+                result2 = a2 + b2;
+            }
+            if (c.equals("-")) {
+                result2 = a2 - b2;
+            }
+            if (c.equals("*")) {
+                result2 = a2 * b2;
+            }
+            if (c.equals("/")) {
+                if (b2 == 0) {
+                    result2 = 0;
+                } else {
+                    result2 = a2 / b2;
+                }
+
+            }
+
+            result = ((new Double(result2)).toString());
+
+            result_TextField.setText(result);
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        // 设置程序显示的界面风格，可以去除
+    try {
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Calculator cal = new Calculator();
+    }
+
+}
+                
